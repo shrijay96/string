4cf32d52c12e0b9adfd96a472e84e105bebf575a
shrijay96 <shrijaykale999@gmail.com>
2025-10-11 09:33:53 +0000

    commit

---
+++ 01str/01arr/array.js
@@ -0,0 +1,37 @@
+// array
+const myArray = [1,2 ,3,4,5]
+const myHeroes = ['Hanuman','Ram','Krishna']
+
+const myArray2 = new Array(1,2,3)
+console.log(myArray[0]);
+
+// ARRAY METHODS
+
+//myArray.push(6)
+//myArray.pop()
+//myArray.unshift(0)
+//myArray.shift()
+console.log(myArray);
+
+//console.log(myArray.includes(4));
+//console.log(myArray.indexOf(3));
+
+const myArr = [0,1,2,3,4,]
+const newArr = myArray.join()
+console.log(myArr);
+console.log(newArr);
+
+// slice, spice
+
+console.log("A",myArr);
+
+constmyn1 =myArr.slice(1,3)
+
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,2)
+console.log("c",myArr);
+console.log(myn2);
+
+
+

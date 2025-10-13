b3689c7262fe3ac83b1fc8bac7c5897276bb11a7
shrijay96 <shrijaykale999@gmail.com>
2025-10-13 09:46:40 +0000

    commit

---
+++ 01str/iteration/two.js
@@ -0,0 +1,19 @@
+let i =0;
+while (i <= 10){
+   // console.log(`value of i is ${i}`);
+    i=i +2
+}
+
+array =["jay","shree","Raghahv","sarayu"];
+
+let arr =0;
+while(arr < array.length){
+    //console.log(`value is ${array[arr]}`);
+    arr = arr +1
+}
+
+let score = 1;
+do{
+    console.log(`score is ${score}`);
+    score++
+}while (score <= 10);
